62f7eef205ace1d5ce8598054a0c570ce65d1fc8
hz <gravatar.m2a16@simplelogin.com>
2024-02-22 13:42:13 +0200

    build to monorepo root

--- apps/friedger/vite.config.ts
+++ apps/friedger/vite.config.ts
@@ -4,5 +4,8 @@ import react from "@vitejs/plugin-react-swc"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/friedger/",
+  base: "/stacks-ogs/friedger/",
+  build: {
+    outDir: "../../dist/friedger",
+  },
 })
